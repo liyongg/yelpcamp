74a0e7551cd93010abb19a7585eb3c9b1114b8e1
Li Yong Pan <michelpan@hotmail.com>
2023-06-27 23:44:33 +0200

    initialising campgrounds database

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
 const express = require('express');
-const app = express();
 const path = require('path');
+const mongoose = require('mongoose');
+const globals = require('./globals');
+const Campground = require('./models/campground');
+
+const dbConnection = globals.dbConnection;
+
+mongoose.connect(dbConnection, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+    console.log("Database connected");
+})
+
+const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
@@ -8,6 +25,12 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
     res.render('home')
+})
+
+app.get('/makecampground', async (req, res) => {
+    const camp = new Campground({ title: 'My Backyard' });
+    await camp.save();
+    res.send(camp)
 })
 
 app.listen(3000, () => {
