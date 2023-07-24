a4caa3e17a3c2dde81177fbc16bc0870906a4951
Li Yong Pan <michelpan@hotmail.com>
2023-07-25 01:08:02 +0200

    add basic review model

--- models/campground.js
+++ models/campground.js
@@ -6,7 +6,13 @@ const CampgoundSchema = new Schema({
     image: String,
     price: Number,
     description: String,
-    location: String
+    location: String,
+    reviews: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Review"
+        }
+    ]
 })
 
 module.exports = mongoose.model('Campground', CampgoundSchema);
