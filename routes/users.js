fb1ac9b6ed5390684305865962e05f0cc1e5bd3c
Li Yong Pan <michelpan@hotmail.com>
2023-08-01 23:48:07 +0200

    login routes

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const router = express.Router();
+const passport = require("passport");
+const catchAsync = require("../utils/catchAsync");
 const User = require("../models/user");
 
 router.get("/register", (req, res) => {
     res.render("users/register")
 })
-router.post("/register", async (req, res) => {
+router.post("/register", catchAsync(async (req, res) => {
     try {
         const { email, username, password } = req.body
         const user = new User({ email, username });
@@ -17,6 +19,16 @@ router.post("/register", async (req, res) => {
         res.redirect("/register")
     }
 
+}))
+
+router.get("/login", (req, res) => {
+    res.render("users/login")
+})
+
+router.post("/login", passport.authenticate("local", { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
+    const { username } = req.body;
+    req.flash("success", `Welcome Back, ${username}!`)
+    res.redirect("/campgrounds")
 })
 
 module.exports = router;
