f1198d0f8ff8a69d0f21257f93224c9437943e05
Li Yong Pan <michelpan@hotmail.com>
2023-08-03 00:35:59 +0200

    reviews controller

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -25,7 +25,6 @@ module.exports.showCampground = async (req, res) => {
             populate: { path: "author" }
         })
         .populate("author");
-    console.log(campground)
     if (!campground) {
         req.flash("error", "Cannot find this campground!");
         return res.redirect("/campgrounds");
