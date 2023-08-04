8747a54416f69e836ab3f7dba6bff60f181e72db
Li Yong Pan <michelpan@hotmail.com>
2023-08-04 17:13:24 +0200

    thumbnail images

--- models/campground.js
+++ models/campground.js
@@ -3,14 +3,18 @@ const Review = require('./review');
 const { array } = require('joi');
 const Schema = mongoose.Schema;
 
+const ImageSchema = new Schema({
+    url: String,
+    filename: String
+})
+
+ImageSchema.virtual("thumbnail").get(function () {
+    return this.url.replace("/upload", "/upload/w_200")
+})
+
 const CampgoundSchema = new Schema({
     title: String,
-    images: [
-        {
-            url: String,
-            filename: String
-        }
-    ],
+    images: [ImageSchema],
     price: Number,
     description: String,
     location: String,
